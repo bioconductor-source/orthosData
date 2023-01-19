e7d2a22277fa2883bf760a8f2d591573e878cb09
ppapasaikas <panp80@gmail.com>
2023-01-19 14:08:00 +0100

    Fill make-metadata.R and create metadata.csv

--- inst/scripts/make-metadata.R
+++ inst/scripts/make-metadata.R
@@ -11,45 +11,159 @@ suppressPackageStartupMessages({
 })
 
 df <- dplyr::bind_rows(
-    ## Mouse ARCHS4
+    ## Mouse Context Encoder
     data.frame(
-        Title = "",
-        Description = "",
+        Title = "ContextEncoder_Mouse",
+        Description = "Context (gene lcpms) encoder of mouse contrast experiments compiled from ARCHS4",
         BiocVersion = "3.17",
-        Genome = "",
-        SourceType = "",  ## check AnnotationHubData::getValidSourceTypes()
-        SourceUrl = "",
-        SourceVersion = "",  ## ARCHS4 version
+        Genome = "GRCm39",
+        SourceType = "HDF5",  ## check AnnotationHubData::getValidSourceTypes()
+        SourceUrl = "https://maayanlab.cloud/archs4/",
+        SourceVersion = "ARCHS4 v2.1.2",  ## ARCHS4 version
         Species = "Mus musculus",
         TaxonomyId = "10090",
         Coordinate_1_based = NA,
-        DataProvider = "",
+        DataProvider = "Ma'ayan Laboratory (https://labs.icahn.mssm.edu/maayanlab/)",
         Maintainer = "Panagiotis Papasaikas <panagiotis.papasaikas@fmi.ch>",
-        RDataClass = "SummarizedExperiment",
-        DispatchClass = "", ## AnnotationHub::DispatchClassList()
+        RDataClass = "keras.engine.functional.Functional",
+        DispatchClass = "keras::load_model_hdf5; require keras", ## AnnotationHub::DispatchClassList()
         Location_Prefix = "",
-        RDataPath = ""
+        RDataPath = "TRAINED_MODELS/ContextEncoder_ARCHS4_v212_Mouse"
     ),
-    ## Human ARCHS4
+    ## Human Context Encoder
     data.frame(
-        Title = "",
-        Description = "",
+        Title = "ContextEncoder_Human",
+        Description = "Context (gene lcpms) encoder of human contrast experiments compiled from ARCHS4",
         BiocVersion = "3.17",
-        Genome = "",
-        SourceType = "",  ## check AnnotationHubData::getValidSourceTypes()
-        SourceUrl = "",
-        SourceVersion = "",  ## ARCHS4 version
+        Genome = "GRCh38",
+        SourceType = "HDF5",  ## check AnnotationHubData::getValidSourceTypes()
+        SourceUrl = "https://maayanlab.cloud/archs4/",
+        SourceVersion = "ARCHS4 v2.1.2",  ## ARCHS4 version
         Species = "Homo sapiens",
         TaxonomyId = "9606",
         Coordinate_1_based = NA,
-        DataProvider = "",
+        DataProvider = "Ma'ayan Laboratory (https://labs.icahn.mssm.edu/maayanlab/)",
         Maintainer = "Panagiotis Papasaikas <panagiotis.papasaikas@fmi.ch>",
-        RDataClass = "SummarizedExperiment",
-        DispatchClass = "", ## AnnotationHub::DispatchClassList()
+        RDataClass = "keras.engine.functional.Functional",
+        DispatchClass = "keras::load_model_hdf5; require keras", ## AnnotationHub::DispatchClassList()
         Location_Prefix = "",
-        RDataPath = ""
+        RDataPath = "TRAINED_MODELS/ContextEncoder_ARCHS4_v212_Mouse"
+    ),
+    ## Mouse Contrast Encoder
+    data.frame(
+        Title = "DeltaEncoder_Mouse",
+        Description = "Contrast (gene lcpm deltas i.e LFCs) encoder of mouse contrast experiments compiled from ARCHS4",
+        BiocVersion = "3.17",
+        Genome = "GRCm39",
+        SourceType = "HDF5",  ## check AnnotationHubData::getValidSourceTypes()
+        SourceUrl = "https://maayanlab.cloud/archs4/",
+        SourceVersion = "ARCHS4 v2.1.2",  ## ARCHS4 version
+        Species = "Mus musculus",
+        TaxonomyId = "10090",
+        Coordinate_1_based = NA,
+        DataProvider = "Ma'ayan Laboratory (https://labs.icahn.mssm.edu/maayanlab/)",
+        Maintainer = "Panagiotis Papasaikas <panagiotis.papasaikas@fmi.ch>",
+        RDataClass = "keras.engine.functional.Functional",
+        DispatchClass = "keras::load_model_hdf5; require keras", ## AnnotationHub::DispatchClassList()
+        Location_Prefix = "",
+        RDataPath = "TRAINED_MODELS/DeltaEncoder_FT_ARCHS4_v212_Mouse"
+    ),
+    ## Human Context Encoder
+    data.frame(
+        Title = "DeltaEncoder_Human",
+        Description = "Contrast (gene lcpm deltas i.e LFCs) encoder of human contrast experiments compiled from ARCHS4",
+        BiocVersion = "3.17",
+        Genome = "GRCh38",
+        SourceType = "HDF5",  ## check AnnotationHubData::getValidSourceTypes()
+        SourceUrl = "https://maayanlab.cloud/archs4/",
+        SourceVersion = "ARCHS4 v2.1.2",  ## ARCHS4 version
+        Species = "Homo sapiens",
+        TaxonomyId = "9606",
+        Coordinate_1_based = NA,
+        DataProvider = "Ma'ayan Laboratory (https://labs.icahn.mssm.edu/maayanlab/)",
+        Maintainer = "Panagiotis Papasaikas <panagiotis.papasaikas@fmi.ch>",
+        RDataClass = "keras.engine.functional.Functional",
+        DispatchClass = "keras::load_model_hdf5; require keras", ## AnnotationHub::DispatchClassList()
+        Location_Prefix = "",
+        RDataPath = "TRAINED_MODELS/DeltaEncoder_FT_ARCHS4_v212_Human"
+    ),
+    ## Mouse Contrast Decoder
+    data.frame(
+        Title = "DeltaDecoder_Mouse",
+        Description = "Contrast (gene lcpm deltas i.e LFCs) decoder of mouse contrast experiments compiled from ARCHS4",
+        BiocVersion = "3.17",
+        Genome = "GRCm39",
+        SourceType = "HDF5",  ## check AnnotationHubData::getValidSourceTypes()
+        SourceUrl = "https://maayanlab.cloud/archs4/",
+        SourceVersion = "ARCHS4 v2.1.2",  ## ARCHS4 version
+        Species = "Mus musculus",
+        TaxonomyId = "10090",
+        Coordinate_1_based = NA,
+        DataProvider = "Ma'ayan Laboratory (https://labs.icahn.mssm.edu/maayanlab/)",
+        Maintainer = "Panagiotis Papasaikas <panagiotis.papasaikas@fmi.ch>",
+        RDataClass = "keras.engine.functional.Functional",
+        DispatchClass = "keras::load_model_hdf5; require keras", ## AnnotationHub::DispatchClassList()
+        Location_Prefix = "",
+        RDataPath = "TRAINED_MODELS/DeltaDecoder_FT_ARCHS4_v212_Mouse"
+    ),
+    ## Human Context Encoder
+    data.frame(
+        Title = "DeltaDecoder_Human",
+        Description = "Contrast (gene lcpm deltas i.e LFCs) decoder of human contrast experiments compiled from ARCHS4",
+        BiocVersion = "3.17",
+        Genome = "GRCh38",
+        SourceType = "HDF5",  ## check AnnotationHubData::getValidSourceTypes()
+        SourceUrl = "https://maayanlab.cloud/archs4/",
+        SourceVersion = "ARCHS4 v2.1.2",  ## ARCHS4 version
+        Species = "Homo sapiens",
+        TaxonomyId = "9606",
+        Coordinate_1_based = NA,
+        DataProvider = "Ma'ayan Laboratory (https://labs.icahn.mssm.edu/maayanlab/)",
+        Maintainer = "Panagiotis Papasaikas <panagiotis.papasaikas@fmi.ch>",
+        RDataClass = "keras.engine.functional.Functional",
+        DispatchClass = "keras::load_model_hdf5; require keras", ## AnnotationHub::DispatchClassList()
+        Location_Prefix = "",
+        RDataPath = "TRAINED_MODELS/DeltaDecoder_FT_ARCHS4_v212_Human"
+    ),
+    
+    ## Mouse ContrastDB Summarized Experiment
+    data.frame(
+        Title = "decomposed_contrasts_mouse",
+        Description = "HDF5-based SummarizedExperiment with full and decomposed mouse contrast experiments compiled from ARCHS4 and associated metadata",
+        BiocVersion = "3.17",
+        Genome = "GRCm39",
+        SourceType = "tar",  ## check AnnotationHubData::getValidSourceTypes()
+        SourceUrl = "https://maayanlab.cloud/archs4/",
+        SourceVersion = "ARCHS4 v2.1.2",  ## ARCHS4 version
+        Species = "Mus musculus",
+        TaxonomyId = "10090",
+        Coordinate_1_based = NA,
+        DataProvider = "Ma'ayan Laboratory (https://labs.icahn.mssm.edu/maayanlab/)",
+        Maintainer = "Panagiotis Papasaikas <panagiotis.papasaikas@fmi.ch>",
+        RDataClass = "keras.engine.functional.Functional",
+        DispatchClass = "HDF5Array::loadHDF5SummarizedExperiment; require HDF5Array", ## AnnotationHub::DispatchClassList()
+        Location_Prefix = "",
+        RDataPath = "DECOMPOSED_CONTRASTS/mouse_v212_c100se.tar"
+    ),
+    ## Human ContrastDB Summarized Experiment
+    data.frame(
+        Title = "decomposed_contrasts_human",
+        Description = "HDF5-based SummarizedExperiment with full and decomposed human contrast experiments compiled from ARCHS4 and associated metadata",
+        BiocVersion = "3.17",
+        Genome = "GRCh38",
+        SourceType = "tar",  ## check AnnotationHubData::getValidSourceTypes()
+        SourceUrl = "https://maayanlab.cloud/archs4/",
+        SourceVersion = "ARCHS4 v2.1.2",  ## ARCHS4 version
+        Species = "Homo sapiens",
+        TaxonomyId = "9606",
+        Coordinate_1_based = NA,
+        DataProvider = "Ma'ayan Laboratory (https://labs.icahn.mssm.edu/maayanlab/)",
+        Maintainer = "Panagiotis Papasaikas <panagiotis.papasaikas@fmi.ch>",
+        RDataClass = "keras.engine.functional.Functional",
+        DispatchClass = "HDF5Array::loadHDF5SummarizedExperiment; require HDF5Array", ## AnnotationHub::DispatchClassList()
+        Location_Prefix = "",
+        RDataPath = "DECOMPOSED_CONTRASTS/human_v212_c100se.tar"
     )
-
 )
 
 write.csv(
